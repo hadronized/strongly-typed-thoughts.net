9621f93669c41d86d4f8aaa7ea1114d68332129b
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-02-17 02:32:21 +0100

    Updated the adduploader sqlite code.

--- db/CreateSchema.hs
+++ db/CreateSchema.hs
@@ -16,8 +16,8 @@ createCredentialsSQL :: String
 createCredentialsSQL = unlines
     [
       "create table Credentials ("
-    , "    credID   Integer primary key"
-    , "  , credName VarChar(64) not null unique"
-    , "  , credPwd  Bit(512) not null"
+    , "    cred_id   Integer primary key"
+    , "  , cred_name VarChar(64) not null unique"
+    , "  , cred_pwd  Bit(512) not null"
     , ");"
     ]
--- utils/adduploader/src/Main.hs
+++ utils/adduploader/src/Main.hs
@@ -1,22 +1,23 @@
-import Database.HDBC
-import Database.HDBC.Sqlite3
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.String ( IsString(..) )
+import Data.Text ( Text )
+import Database.SQLite.Simple ( Query, execute, withConnection, withTransaction )
 import Password ( hashPwd )
 import System.Environment ( getArgs )
 import System.IO ( hPutStrLn, stderr )
 
 main :: IO ()
 main = do
-    args <- getArgs
-    if length args == 2
-    then do
-      let [login,pwd] = args
-      putStrLn "connecting to database..."
-      conn <- connectSqlite3 "db/local.db"
-      run conn addUserSQL [SqlString login,SqlByteString $ hashPwd pwd]
-      commit conn
-      disconnect conn
-    else
-      hPutStrLn stderr "syntax error"
+  args <- getArgs
+  if length args == 2
+  then do
+    let [login,pwd] = args
+    putStrLn "connecting to database..."
+    withConnection "db/local.db" $ \conn -> withTransaction conn $ do
+      execute conn addUserQuery (fromString login :: Text, hashPwd pwd)
+  else
+    hPutStrLn stderr "syntax error"
 
-addUserSQL :: String
-addUserSQL = "insert into Credentials (credName,CredPwd) VALUES (?,?)"
+addUserQuery :: Query 
+addUserQuery = "insert into Credentials (cred_name, cred_pwd) values (?,?)"
