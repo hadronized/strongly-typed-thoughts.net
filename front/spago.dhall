5381d70f6e0d406e4538b0c6ec858eb531362574
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2021-08-27 00:34:12 +0200

    Make blog articles readable.

--- front/spago.dhall
+++ front/spago.dhall
@@ -26,6 +26,7 @@ to generate this file without the comments in this block.
   , "foreign-object"
   , "formatters"
   , "halogen"
+  , "html-parser-halogen"
   , "integers"
   , "js-date"
   , "lists"
--- front/src/component/Blog.purs
+++ front/src/component/Blog.purs
@@ -3,14 +3,15 @@ module Blog where
 
 import Prelude
 
+import API (endpoint)
 import Affjax as AX
-import Affjax.ResponseFormat (json)
-import Control.Monad.RWS (put)
+import Affjax.ResponseFormat (json, string)
+import Control.Monad.RWS (gets, modify_, put)
 import Data.Argonaut.Core (Json, caseJsonArray, caseJsonObject, toArray, toString)
 import Data.Array as A
-import Data.Bifunctor (lmap)
+import Data.Bifunctor (bimap, lmap)
 import Data.DateTime (DateTime)
-import Data.Either (Either(Left, Right))
+import Data.Either (Either(Left, Right), either)
 import Data.Formatter.DateTime (FormatterCommand(..))
 import Data.Formatter.DateTime as DT
 import Data.List as L
@@ -24,13 +25,16 @@ import Effect.Class (liftEffect)
 import Effect.Class.Console (log)
 import Foreign.Object (Object, lookup)
 import HTMLHelper (cl)
-import Halogen (Component, defaultEval, mkComponent, mkEval)
-import Halogen.HTML (a, b_, blockquote_, button, em_, h1, h2, hr_, i, p_, section, span, text)
+import Halogen (Component, HalogenM, defaultEval, mkComponent, mkEval)
+import Halogen.HTML (PlainHTML, a, b_, blockquote_, button, em_, fromPlainHTML, h1, h2, hr_, i, p_, section, span, text)
 import Halogen.HTML as H
+import Halogen.HTML.Events (onClick)
 import Halogen.HTML.Properties (href)
+import Html.Renderer.Halogen as RH
 
 data Action
   = Init
+  | ReadArticle Slug
 
 newtype Slug
   = Slug String
@@ -41,14 +45,18 @@ instance Eq Slug where
 instance Ord Slug where
   compare (Slug a) (Slug b) = compare a b
 
+instance Show Slug where
+  show (Slug s) = s
+
 type Article
   =
-    { metadata :: Metadata, content :: Maybe String
+    { metadata :: Metadata, content :: Maybe PlainHTML
     }
 
 type State
   =
     { articles :: Map Slug Article
+    , currentArticle :: Maybe PlainHTML
     }
 
 blogComponent :: forall query input output m. (MonadAff m) => Component query input output m
@@ -58,28 +66,33 @@ blogComponent = mkComponent { eval, initialState, render }
 
   handleAction = case _ of
     Init -> do
-      response <- liftAff <<< map ((\res -> metadataFromJson res.body) <=< lmap AX.printError) $ AX.get json "/api/v1/blog"
+      response <- liftAff <<< map ((\res -> metadataFromJson res.body) <=< lmap AX.printError) <<< AX.get json $ endpoint "/blog"
       case response of
         Left e -> liftEffect $ log e
         Right metadata -> do
-          liftEffect <<< log $ show metadata
           let
             articles :: Map Slug Article
             articles =
               M.fromFoldable
                 $ map
                     ( \m ->
-                        Tuple (Slug m.slug)
+                        Tuple m.slug
                               { metadata: m, content: Nothing
                               }
                     )
                     metadata
-          put $ { articles }
+            currentArticle = Nothing
+          put $ { articles, currentArticle }
 
-  initialState _ = { articles: M.empty }
+    ReadArticle slug -> readArticle slug
 
-  render state =
-    section [ cl [ "container", "section", "content" ] ]
+  initialState _ = { articles: M.empty, currentArticle: Nothing }
+
+  render state = section [ cl [ "container", "section", "content" ] ] $ case state.currentArticle of
+    Just html -> [fromPlainHTML html]
+    Nothing -> renderListing state
+
+  renderListing state =
       ([ h1 [ cl [ "title" ] ]
           [ b_ [ text "Dimitri Sabadie" ], text "’s blog"
           ]
@@ -108,7 +121,7 @@ blogComponent = mkComponent { eval, initialState, render }
 
   inlineBlogArticle article = H.div [cl ["level"]] [
     span [cl ["level-left"] ] [span [cl ["level-item"]][
-      a [href $ "/blog/" <> article.metadata.slug] [text article.metadata.name]
+      a [onClick <<< const $ ReadArticle article.metadata.slug] [text article.metadata.name]
     ]],
     span [cl ["level-right"] ] [span [cl ["level-item"]][
       em_ [text $ "on " <> formatTime article.metadata.publishDate ]
@@ -119,7 +132,7 @@ type Metadata
   = { name :: String
     , publishDate :: DateTime
     , tags :: Array String
-    , slug :: String
+    , slug :: Slug
     }
 
 -- | Convert a Json value into a list of article metadata.
@@ -135,7 +148,7 @@ metadataFromJson = caseJsonArray (Left "payload is not an array of metadata") (t
     publishDateRaw <- extractField "missing article publish date" "article_publish_date" toString
     publishDate <- DT.unformatDateTime timeFormat publishDateRaw
     tags <- extractField "missing article tags" "article_tags" $ toArray >=> traverse toString
-    slug <- extractField "missing article slug" "article_slug" toString
+    slug <- extractField "missing article slug" "article_slug" (map Slug <<< toString)
     pure { name, publishDate, tags, slug }
     where
     extractField :: forall a. String -> String -> (Json -> Maybe a) -> Either String a
@@ -148,3 +161,28 @@ formatTime = DT.format fmt
   where fmt = L.fromFoldable [
      DayOfWeekNameShort, Placeholder " ", DayOfMonthTwoDigits, Placeholder " ", MonthShort, Placeholder " ",
     YearFull, Placeholder ", ", Hours24, Placeholder ":", MinutesTwoDigits, Placeholder " UTC"]
+
+-- | Retrieve the content of a blog article.
+getArticleContent :: forall m. MonadAff m => Slug -> m (Either String PlainHTML)
+getArticleContent (Slug slug) =
+  liftAff <<< map (bimap AX.printError (RH.render_ <<< _.body)) <<< AX.get string $ endpoint "/blog/" <> slug
+
+-- | Read an article.
+readArticle :: forall slots output m. MonadAff m => Slug -> HalogenM State Action slots output m Unit
+readArticle slug = do
+    article <- gets (M.lookup slug <<< _.articles)
+    maybe cacheAndSwitch switchToArticle $ article >>= _.content
+  where
+    -- read and cache, then switch
+    cacheAndSwitch = getArticleContent slug >>= either noSuchArticle cache
+
+    cache html = do
+      modify_ $ \state -> state
+        { articles = M.update (\article -> Just article { content = Just html }) slug state.articles }
+      switchToArticle html
+
+    noSuchArticle _ = pure unit -- FIXME
+
+    switchToArticle html = do
+      modify_ $ \state -> state { currentArticle = Just html }
+      -- TODO: switch the location in the url
